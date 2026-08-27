f5e0a8778b0cf4563658fb1c660af8ff92b92593
Sarthak Chaudhari <codeSarthak02@gmail.com>
2026-08-27 14:45:58 +0530

    Add Google search Selenium test

--- src/test/java/tests/ExampleTest.java
+++ src/test/java/tests/ExampleTest.java
@@ -3,9 +3,11 @@ package tests;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ExampleTest {
@@ -26,6 +28,23 @@ public class ExampleTest {
         System.out.println("Page Title: " + title);
 
         assertTrue(title.toLowerCase().contains("google"));
+    }
+
+    @Test
+    void testGoogleSearch() {
+        driver.get("https://www.google.com");
+
+        driver.findElement(By.name("q"))
+                .sendKeys("Jenkins");
+
+        driver.findElement(By.name("btnK"))
+                .click();
+
+        String title = driver.getTitle();
+
+        System.out.println("Search Page Title: " + title);
+
+        assertTrue(title.toLowerCase().contains("jenkins"));
     }
 
     @AfterAll
